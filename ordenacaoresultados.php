a705b947fcd8158f50e20c76886c1129c805a405
Itamar Simão <102179953+ItaSimao@users.noreply.github.com>
2025-04-10 06:46:51 -0300

    finalizado

--- ordenacaoresultados.php
+++ ordenacaoresultados.php
@@ -12,39 +12,46 @@ try {
     $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Erro na conexão: " . $e->getMessage());
+    echo ("Erro na conexão: " . $e->getMessage());
+}
+
+if (isset($_GET['ordem']) && empty($_GET['ordem']) == false) {
+    $ordem = addslashes($_GET['ordem']);
+    $sql = "SELECT * FROM usuarios ORDER BY " . $ordem;
+} else {
+    $ordem = '';
+    $sql = "SELECT * FROM usuarios";
 }
 
 ?>
 <form method="GET">
     <select name="ordem" onchange="this.form.submit()">
-<option></option>
-<option value="nome">Nome</option>
-<option value="idade">Idade</option>
-</select>
+        <option></option>
+        <option value="nome" <?php echo($ordem=="nome")?'selected="selected"':"";  ?> >Nome</option>
+        <option value="idade"<?php echo($ordem=="idade")?'selected="selected"':""; ?> >Idade </option>
+    </select>
 </form>
 
 <table border="1" width="400">
     <tr>
-    <th>Nome</th>
-    <th>Idade</th>
+        <th>Nome</th>
+        <th>Idade</th>
     </tr>
-    
-    <?php
-    $sql = "SELECT * from usuarios";
-    $sql = $pdo->query($sql);
-    if($sql->rowCount() > 0) {
 
-        foreach($sql->fetchAll() as $usuarios):
+    <?php
+    $sql = $pdo->query($sql);
+    if ($sql->rowCount() > 0) {
+
+        foreach ($sql->fetchAll() as $usuarios):
             ?>
             <tr>
                 <td><?php echo $usuarios['nome']; ?></td>
                 <td><?php echo $usuarios['idade']; ?></td>
             </tr>
             <?php
-            endforeach;
-        } else {
-            echo "<tr><td colspan='2'>Nenhum usuário encontrado</td></tr>";
-        }
-        ?>
+        endforeach;
+    } else {
+        echo "<tr><td colspan='2'>Nenhum usuário encontrado</td></tr>";
+    }
+    ?>
 </table>
